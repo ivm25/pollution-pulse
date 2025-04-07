9663b1362c9aeac9467ee51343e7b80b87fa5c83
Ishan Mahajan <ishanvm25@gmail.com>
2025-04-07 19:58:40 +1000

    added anomaly detection weekly function
    weekly anomaly detection

--- data_manipulation/data_wrangling.py
+++ data_manipulation/data_wrangling.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sktime
 import pytimetk
 from pytimetk import summarize_by_time
+from pytimetk import anomalize
 import plotly
 import os
 
@@ -26,3 +27,27 @@ def summary_by_time(df, key1,key2):
     return data_by_category
 
 
+def summary_by_time_weekly(df, key1,key2):
+
+    data_by_category = df\
+                    .groupby([key1,key2])\
+                    .summarize_by_time(
+                        date_column = 'Date',
+                        value_column = 'Value',
+                        freq = "W",
+                        agg_func = 'mean',
+                        wide_format = False,
+                        engine = "polars"
+                    )
+    
+    return data_by_category
+
+
+def anamoly_detection(df, key1, key2):
+
+    anomalised_df = df.groupby([key1, key2], sort = False)\
+                      .anomalize(date_column = "Date",
+                                 value_column = 'Value_mean',
+                                 period = 6)
+    
+    return anomalised_df

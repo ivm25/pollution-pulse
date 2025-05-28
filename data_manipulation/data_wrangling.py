2cf546512c488bf577490cd47ff5362aab454d73
Ishan Mahajan <ishanvm25@gmail.com>
2025-05-28 15:01:32 +1000

    Update data_wrangling.py
    Added functions to summarize data for air quality category and function to manipulate data for classification tasks

--- data_manipulation/data_wrangling.py
+++ data_manipulation/data_wrangling.py
@@ -27,6 +27,25 @@ def summary_by_time(df, key1,key2):
     return data_by_category
 
 
+def air_category_summary_by_time(df, key1,
+                                 key2,
+                                 key3):
+
+    data_by_category = df\
+                    .groupby([key1,key2,key3])\
+                    .summarize_by_time(
+                        date_column = 'Date',
+                        value_column = 'Value',
+                        freq = "W",
+                        agg_func = 'mean',
+                        wide_format = False,
+                        engine = "polars"
+                    )
+    
+    return data_by_category
+
+
+
 def summary_by_time_weekly(df, key1,key2):
 
     data_by_category = df\
@@ -51,3 +70,19 @@ def anamoly_detection(df, key1, key2):
                                  period = 6)
     
     return anomalised_df
+
+def data_for_classification(df,
+                            var_to_model = 'PM10'):
+
+    key_data = df[['Site_Id',
+                   'Date',
+                    'Value',
+                    'AirQualityCategory',
+                    'Parameter_ParameterDescription']]
+    
+    key_data = key_data[key_data['Parameter_ParameterDescription'] != 'Temperature']
+
+    key_data = key_data[key_data['Parameter_ParameterDescription'] == var_to_model]
+    
+    return key_data
+
